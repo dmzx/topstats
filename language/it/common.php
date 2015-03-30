a2c184f006e99ad96976a20fd4df51c5ae84d027
dmzx <info@dmzx-web.net>
2015-03-30 21:12:05 +0200

    update
    update

--- language/it/common.php
+++ language/it/common.php
@@ -9,7 +9,6 @@
 *
 */
 
-
 /**
 * DO NOT CHANGE
 */
@@ -54,19 +53,19 @@ $lang = array_merge($lang, array(
 	'STOP_SCROLL'				=> 'Stop',
 	'LAST_REGISTERED_USERS'		=> 'Ultimi utenti registrati',
 	'LAST_VISITED_BOTS'			=> 'Ultimi bots in visita',
-	
+
 	'TOP_STATS'								=> 'Top Stats',
 	'TOPSTATS_SAVED'						=> 'Impostazioni Top Stats salvate',
 	'TS_VERSION'							=> 'Versione',
-	'TS_DONATE'							    => 'Autore <a href="http://www.phpbb3bbcodes.com"><strong>Stoker</strong></a>. Estensione creata da <a href="http://www.dmzx-web.net"><strong>dmzx</strong></a>',
+	'TS_DONATE'								=> 'Autore <a href="http://www.phpbb3bbcodes.com"><strong>Stoker</strong></a>. Estensione creata da <a href="http://www.dmzx-web.net"><strong>dmzx</strong></a>',
 	'TS_CONFIG'								=> 'Configurazione',
 	'TS_RECENT_SETTINGS'					=> 'Impostazioni argomenti attivi recenti',
 	'TS_RECENT_ENABLE'						=> 'Abilita argomenti attivi recenti',
-	'TS_RECENT_ENABLE_EXPLAIN'				=> 'Attiva o disattiva la visualizzazione del blocco Argomenti recenti',	
-	
+	'TS_RECENT_ENABLE_EXPLAIN'				=> 'Attiva o disattiva la visualizzazione del blocco Argomenti recenti',
+
 	'TSRAT_NUMBER'							=> 'Discussioni recenti attive',
 	'TSRAT_NUMBER_EXPLAIN'					=> 'Numero degli Argomenti recenti attivi da visualizzare',
-	
+
 	'TS_JSSCROLL'							=> 'Scorrimento Jquery',
 	'TS_JSSCROLL_EXPLAIN'					=> 'Attiva o disattiva l’utilizzo dello scorrimento jquery per le Discusioni recenti attive<br />Si prega di notare che questa funzione deve essere disabilitato se si utilizza Jquery Ticker',
 	'TS_JSSCROLL_SPEED'						=> 'Velocità scorrimento Jquery',
@@ -83,37 +82,37 @@ $lang = array_merge($lang, array(
 	'TS_JSSCROLL_NAVIGATION_EXPLAIN'		=> 'Attiva o disattiva la navigazione JSSCroll per Discussione Recenti Attivi',
 	'TS_TICKER_ENABLE'						=> 'Jquery Ticker',
 	'TS_TICKER_ENABLE_EXPLAIN'				=> 'Attivare o disattivare il Jquery Ticker per Discussione Recenti Attivi<br />Si prega di notare che questa funzione deve essere disabilitata se si utilizza lo scorrimento Jquery',
-	
+
 	'TS_MOSTVIEWED_SETTINGS'				=> 'Impostazioni Argomenti più visti',
 	'TS_MOSTVIEWED_ENABLE'					=> 'Abilita Argomenti più visti',
 	'TS_MOSTVIEWED_ENABLE_EXPLAIN'			=> 'Attiva o disattiva la visualizzazione del blocco Argomenti più visti',
 	'TS_MOSTVIEWED_NUMBER'					=> 'Argomenti più visti',
 	'TS_MOSTVIEWED_NUMBER_EXPLAIN'			=> 'Numero degli Argomenti più visti da visualizzare',
-	
+
 	'TS_MOSTREPLIED_SETTINGS'				=> 'Impostazioni Argomenti più risposti',
 	'TS_MOSTREPLIED_ENABLE'					=> 'Abilita Argomenti più risposti',
 	'TS_MOSTREPLIED_ENABLE_EXPLAIN'			=> 'Attiva o disattiva la visualizzazione del blocco Argomenti più risposti',
 	'TS_MOSTREPLIED_NUMBER'					=> 'Argomenti più risposti',
 	'TS_MOSTREPLIED_NUMBER_EXPLAIN'			=> 'Numero degli Argomenti più risposti da visualizzare',
-	
+
 	'TS_MOSTACTIVEUSER_SETTINGS'			=> 'Impostazioni utenti più attivi',
 	'TS_MOSTACTIVEUSER_ENABLE'				=> 'Abilita utenti più attivi',
 	'TS_MOSTACTIVEUSER_ENABLE_EXPLAIN'		=> 'Attiva o disattiva la visualizzazione del blocco utenti più attivi',
 	'TS_MOSTACTIVEUSER_NUMBER'				=> 'Utenti più attivi',
 	'TS_MOSTACTIVEUSER_NUMBER_EXPLAIN'		=> 'Numero degli utenti più attivi da visualizzare',
-	
+
 	'TS_MOSTACTIVEFORUM_SETTINGS'			=> 'Impostazioni forum più attivo',
 	'TS_MOSTACTIVEFORUM_ENABLE'				=> 'Abilita Forum più attivi',
 	'TS_MOSTACTIVEFORUM_ENABLE_EXPLAIN'		=> 'Attiva o disattiva la visualizzazione del blocco Forum più attivi',
 	'TS_MOSTACTIVEFORUM_NUMBER'				=> 'Forum più attivi',
 	'TS_MOSTACTIVEFORUM_NUMBER_EXPLAIN'		=> 'Numero dei Forum più attivi da visualizzare',
-	
+
 	'TS_LASTVISITEDBOT_SETTINGS'			=> 'Impostazioni ultimi bot in visita',
 	'TS_LASTVISITEDBOT_ENABLE'				=> 'Abilita ultimi bot in visita',
 	'TS_LASTVISITEDBOT_ENABLE_EXPLAIN'		=> 'Attiva o disattiva la visualizzazione del blocco ultimi bot in visita',
 	'TS_LASTVISITEDBOT_NUMBER'				=> 'Ultimi bot in visita',
 	'TS_LASTVISITEDBOT_NUMBER_EXPLAIN'		=> 'Numero degli Ultimi bot in visita da visualizzare',
-	
+
 	'TS_LASTREGISTEREDUSER_SETTINGS'		=> 'Impostazioni ultimi utenti registarati',
 	'TS_LASTREGISTEREDUSER_ENABLE'			=> 'Abilita ultimi utenti registrati',
 	'TS_LASTREGISTEREDUSER_ENABLE_EXPLAIN'	=> 'Attiva o disattiva la visualizzazione del blocco ultimi utenti registrati',
